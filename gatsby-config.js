87c826222cf79b64fa3363256f2a11d146b3f402
Stefan Guggisberg <guggiste@students.zhaw.ch>
2019-03-09 21:56:43 +0100

    convert to typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,8 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    'gatsby-plugin-styled-components'`gatsby-plugin-sharp`,
+    'gatsby-plugin-styled-components',
+    `gatsby-plugin-sharp`,
     'gatsby-plugin-typescript',
     {
       resolve: `gatsby-plugin-manifest`,
